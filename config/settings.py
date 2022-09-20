b5ff3ff25cbeaf7a378d5d11a8b63f1eae732b59
simchodi <simchodi@gmail.com>
2022-09-20 04:56:45 +0000

    staticfiles TEST

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import environ
 import os
 
+#test
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -131,7 +133,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# STATIC 파일들이 전부 STATIC_ROOT로 모임
+# STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+	STATIC_DIR,
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
